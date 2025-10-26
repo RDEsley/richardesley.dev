8663b8bd00a9924b2d17c064e86956e0726a50e9
Richard <richardesleyso@gmail.com>
2025-10-26 13:02:24 -0300

    Update: 1.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,16 @@
 // Lista de certificados e cursos
 const certificados = [
   // **Certificados Prioritários**
-  { nome: "Desenvolvimento de APIS em Python com Flask", instituicao: "DIO", ano: 2024 },
   { nome: "Python: Avançando na Linguagem", instituicao: "Alura", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso Python_ avançando na linguagem - Alura.pdf" },
   { nome: "Boas Práticas em Python: Pacotes, Banco de Dados e Desenvolvimento Web", instituicao: "DIO", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso Boas práticas em Python.pdf" },
   { nome: "Curso de Banco de Dados Relacionais com Python DB API", instituicao: "DIO", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso Explorando Banco de dados relacionais com Python DB API.pdf" },
   { nome: "Curso de modelagem de dados: Entidades, Relacionamentos e Atributos", instituicao: "Alura", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso Modelagem de banco de dados_ entidades, relacionamentos e atributos - Alura.pdf" },
   { nome: "Curso Conhecendo a administração do Github", instituicao: "DIO", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso Administração do Github.pdf" },
   { nome: "Curso Utilizando as Ferramentas do Github para solucionar Algoritmos em Python", instituicao: "DIO", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso Utilizando Ferramentas Git.pdf" },
+  { nome: "Desenvolvimento de APIS em Python com Flask", instituicao: "DIO", ano: 2024 },
   // **Desenvolvimento Web e Mobile**
   { nome: "Curso crie um site completo com HTML, CSS e JavaScript", instituicao: "Udemy", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso Crie um site completo HTML, CSS e JS.pdf" },
+  { nome: "Curso crie Site Profissional e Loja Virtual WordPress", instituicao: "Udemy", ano: 2025, url: "src\pdfs\Richard Esley Silva Oliveira - Site Profissional e Loja Virtual WordPress.pdf" },
   { nome: "Curso de Desenvolvimento Android Completo 2024 - Kotlin & Java", instituicao: "Udemy", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso Desenvolvimento Android Completo 2024 - Kotlin e Java.pdf" },
   { nome: "Curso de HTML e CSS - Desenvolvimento", instituicao: "Alura", ano: 2023, url: "src/pdfs/Richard Esley Silva Oliveira - Curso HTML e CSS_ ambientes de desenvolvimento, estrutura de arquivos e tags - Alura.pdf" },
   { nome: "Desenvolva um Jogo e Pratique lógica de Programação", instituicao: "Alura", ano: 2024, url: "src/pdfs/Richard Esley Silva Oliveira - Curso JavaScript e HTML_ desenvolva um jogo e pratique lógica de programação - Alura.pdf" },
