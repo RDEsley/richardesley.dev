661aef954eb8be6a1bce8232e5f2c7fbe9ceb282
RDEsley <richardesleyso@gmail.com>
2026-03-09 20:49:27 -0300

    Adicionando e reordenando os projetos.

--- src/data/projetos.js
+++ src/data/projetos.js
@@ -5,14 +5,13 @@
 
 const PROJETOS = [
   {
-    titulo: "GMC (Granja Mult Core)",
-    status: "Em Desenvolvimento",
-    statusDate: true, // adiciona data atual automaticamente (YYYY.MM.DD)  
-    descricao:
-      "Sistema completo para gestão de granjas, controle de insumos, produção e relatórios em Dashboard.",
-    imagem: "assets/images/granja-mult-core.png", 
-    tech: ["Angular", "Typescript", "Supabase", "HTML/CSS", "Dashboard"],
-    site: "https://gmc-granja-mult-core.vercel.app/login",
+    titulo: "WhatsApp - App de Envio de Mensagens",
+    status: "Finalizado",
+    statusDate: false, 
+    descricao: "Uma replica do App WhatsApp com funcionalidades básicas.",
+    imagem: "assets/images/whatsapp.png",
+    tech: ["Android Studio", "Java", "Kotlin", "XML", "Firebase", "Firebase Auth", "Firebase Database", "Firebase Storage"],
+    repo: "https://github.com/RDEsley/App_WhatsApp"
   },
   {
     titulo: "Fate Eight Tech",
@@ -25,13 +24,23 @@ const PROJETOS = [
     site: "https://fateeight.com.br",
   },
   {
+    titulo: "GMC (Granja Mult Core)",
+    status: "Em Desenvolvimento",
+    statusDate: true, // adiciona data atual automaticamente (YYYY.MM.DD)  (muito legal)
+    descricao:
+      "Sistema completo para gestão de granjas, controle de insumos, produção e relatórios em Dashboard.",
+    imagem: "assets/images/granja-mult-core.png", 
+    tech: ["Angular", "Typescript", "Supabase", "HTML/CSS", "Dashboard"],
+    site: "https://gmc-granja-mult-core.vercel.app/login",
+  },
+  {
     titulo: "Purple Kaizen - SaaS de Gestão de Clientes",
     status: "Em desenvolvimento",
     statusDate: true, 
-    descricao: "Sistema para gerenciar diversos clientes de forma prática e organizada.",
+    descricao: "Sistema para gerenciar clientes de uma empresa de forma prática e organizada.",
     imagem: "assets/images/purple-kaizen.png",
     tech: ["React", "Vite", "Typescript", "Node.js", "Express", "PostgreSQL"],
     repo: "https://github.com/RDEsley/PurpleKaizen",
-    site: "https://purple-kaizen.vercel.app/",
-  }
+  },
+
 ];
